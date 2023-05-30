53c2b53cbf7e23d0d2d15f95d9974a6fbcc562c7
podecafezz <cafeinado97@gmail.com>
2023-05-30 20:59:43 -0300

    api

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//ViewEngine - O que vai ser exibido 
+app.set('view engine','ejs');
+
+
+//carregando arquivos estaticos
+app.use(express.static('public'));
+
+//body-parser - Para poder trabalhar com formulários
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// conect to database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Banco de Dados OK!");
+    }).catch((error) => {
+        console.log( error);
+    })
+
+//exibindo a view 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("Servidor Iniciado!")
+})
